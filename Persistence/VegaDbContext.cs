e47e43ed32653fa77cbbfabbc189fb5ba30d53fb
Rob <rob.fuller94@gmail.com>
2024-01-31 10:01:58 -0600

    Setting up Database/Context/Config

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Persistence
 {
     public class VegaDbContext: DbContext
     {
-        public VegaDbContext(DbContextOptions<VegaDbContext> options) 
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
         : base(options)
-        {
+        { 
+
+        }
+
+            public DbSet<Make> Makes { get; set; }
+            public DbSet<Model> Models { get; set; }
 
         }
     }
-}
