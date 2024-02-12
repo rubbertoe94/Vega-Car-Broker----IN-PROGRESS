a0f52fe004d6c3e6722b1cfc2ecdbd77b3079c8e
Rob <rob.fuller94@gmail.com>
2024-02-12 10:53:44 -0600

    Migration Now Fixed

---
+++ Migrations/20240212164752_RemoveMakeFromVehicle.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vega.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMakeFromVehicle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Makes_MakeId",
+                table: "Vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vehicles_MakeId",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "MakeId",
+                table: "Vehicles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MakeId",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_MakeId",
+                table: "Vehicles",
+                column: "MakeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Makes_MakeId",
+                table: "Vehicles",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+    }
+}
